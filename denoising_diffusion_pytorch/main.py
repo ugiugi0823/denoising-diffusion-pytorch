45acf2e9d8422a6de972b06f040bb1d89f0b3c6d
Hyunwook Jo <acerghjk@gmail.com>
2023-07-13 17:43:17 +0900

    Update main.py

--- denoising_diffusion_pytorch/main.py
+++ denoising_diffusion_pytorch/main.py
@@ -8,17 +8,17 @@ model = Unet(
 
 diffusion = GaussianDiffusion(
     model,
-    image_size = 128,
+    image_size = 64,
     timesteps = 1000,           # number of steps
     sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
 trainer = Trainer(
     diffusion,
-    'path/to/your/images',
+    '/media/oem/VCL_8TB1/VCL/Hyunwook/Data/wta/',
     train_batch_size = 32,
-    train_lr = 8e-5,
-    train_num_steps = 700000,         # total training steps
+    train_lr = 1e-4,
+    train_num_steps = 100000,         # total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
     amp = True,                       # turn on mixed precision
